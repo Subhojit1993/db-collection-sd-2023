1fe2183c9f22b2f53b7b822611f3d758eadcec3f
Subhojit1993 <subho.deb93@gmail.com>
2023-07-22 18:41:55 +0530

    updated app js

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require('cors')
+const cors = require('cors');
 
 // set middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
-app.use(cors())
+});
 
 // import routes
 const routes = require('./routes/Routes');
