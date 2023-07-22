c219deaf2878ead75e23a2d1b8f00826d1e8ad5a
Subhojit1993 <subho.deb93@gmail.com>
2023-07-22 18:34:18 +0530

    updated package

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
 
 // set middleware
 app.use(bodyParser.json());
@@ -13,6 +14,8 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 // import routes
 const routes = require('./routes/Routes');
