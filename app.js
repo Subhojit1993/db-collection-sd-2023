521b477478ea854cc6e8088d51f0f4ee8c38c037
Subhojit1993 <subho.deb93@gmail.com>
2023-07-22 17:21:06 +0530

    updated app js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3000;
 // set middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // import routes
 const routes = require('./routes/Routes');
