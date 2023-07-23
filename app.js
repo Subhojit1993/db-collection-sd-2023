cca9d3ac1adabd97b74ac5dd89b52e68474d1ceb
Subhojit1993 <subho.deb93@gmail.com>
2023-07-23 06:25:51 +0530

    re-updated app

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 // set middleware
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
