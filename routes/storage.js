9c0ba4761e680cc1f4a433a5e1dfec3a0737e2b0
Subhojit1993 <subho.deb93@gmail.com>
2023-07-23 08:45:34 +0530

    updated storage js

--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const storageRoutes = express.Router();
-const fs = require('fs');
-const storagePath = './public/storage.json';
-const { getData, saveData } = require('../utils/common');
-const storageUrl = '/sd-db-1021/collections/:type';
+const fs = require("fs");
+const storagePath = "./public/storage.json";
+const { getData, saveData } = require("../utils/common");
+const storageUrl = "/sd-db-1021/collections/:type/";
 
 storageRoutes.post(storageUrl, (req, res) => {
     const { type } = req.params;
@@ -25,7 +25,7 @@ storageRoutes.post(storageUrl, (req, res) => {
     let displayResponse = element;
     // send the success response
     res.send(displayResponse);
-})
+});
 
 storageRoutes.get(storageUrl, (req, res) => {
     const { type } = req.params;
@@ -35,83 +35,96 @@ storageRoutes.get(storageUrl, (req, res) => {
     // get the existing data-set fot the specific type
     let existingCollectionTypeData = [...existingDataCollection[type]];
 
-    console.log("quer", quer, type);
-
     // search filter implementation based on query
     if (quer) {
         // filter query
         const query = quer.toLowerCase().replaceAll(" ", "");
-        console.log("query", query);
         // get the data-object related to the specified type
-        existingCollectionTypeData = existingCollectionTypeData.filter(element => element.name.replaceAll(" ", "").toLowerCase().includes(query));
+        existingCollectionTypeData = existingCollectionTypeData.filter((element) =>
+            element.name.replaceAll(" ", "").toLowerCase().includes(query)
+        );
     }
 
     // send the response with the collection of specified types
     res.send(existingCollectionTypeData);
-})
+});
 
 // Storage update query
-storageRoutes.patch(`${storageUrl}/:id`, (req, res) => {
+storageRoutes.patch(`${storageUrl}:id`, (req, res) => {
     const { id, type } = req.params;
     // get existing data collection
     let existingDataCollection = getData(storagePath);
     // read the file from the system
-    fs.readFile(storagePath, 'utf-8', (_error, _data) => {
+    fs.readFile(storagePath, "utf-8", (_error, _data) => {
         // get the existing data-set fot the specific type
         let existingCollectionTypeData = [...existingDataCollection[type]];
         // get the data-object related to the specified type
-        let existingData = existingCollectionTypeData.find(element => element._id === parseInt(id));
+        let existingData = existingCollectionTypeData.find(
+            (element) => element._id === parseInt(id)
+        );
         // if no data matching the product then send failed response status with the message
-        if (!existingData) return res.status(400).send({ error: true, msg: `Unable to update the ${type}` })
+        if (!existingData)
+            return res
+                .status(400)
+                .send({ error: true, msg: `Unable to update the ${type}` });
         // get the modified existing data
         let modifiedExistingData = { ...existingData, ...req.body };
         // map the existing collection data-set related to the specified type
-        existingCollectionTypeData = existingCollectionTypeData.map(data => data._id === existingData._id ? modifiedExistingData : data);
+        existingCollectionTypeData = existingCollectionTypeData.map((data) =>
+            data._id === existingData._id ? modifiedExistingData : data
+        );
         // set back the existing data collection with respect to the specified type
         existingDataCollection[type] = existingCollectionTypeData;
-        // save the data 
+        // save the data
         saveData(storagePath, existingDataCollection);
         // set the display response
-        let displayResponse = new Object;
+        let displayResponse = new Object();
         // Initialize the object of type
-        displayResponse[type] = new Object;
+        displayResponse[type] = new Object();
         // update with modified existing data
         displayResponse[type][id] = modifiedExistingData;
         // send the response to the client
         res.send(displayResponse);
-    })
-})
+    });
+});
 
 // Delete query
-storageRoutes.delete(`${storageUrl}/:id`, (req, res) => {
+storageRoutes.delete(`${storageUrl}:id`, (req, res) => {
     const { id, type } = req.params;
     // get existing data collection
     let existingDataCollection = getData(storagePath);
     // read the file from the system
-    fs.readFile(storagePath, 'utf-8', (_error, _data) => {
+    fs.readFile(storagePath, "utf-8", (_error, _data) => {
         // get the existing data-set fot the specific type
         let existingCollectionTypeData = [...existingDataCollection[type]];
         // get the data-object related to the specified type
-        let deletedData = existingCollectionTypeData.find(element => element._id === parseInt(id));
+        let deletedData = existingCollectionTypeData.find(
+            (element) => element._id === parseInt(id)
+        );
 
         // if no data matching the product then send failed response status with the message
-        if (!deletedData) return res.status(400).send({ error: true, msg: `Unable to delete the ${type}` })
+        if (!deletedData)
+            return res
+                .status(400)
+                .send({ error: true, msg: `Unable to delete the ${type}` });
         // filter collection data
-        existingCollectionTypeData = existingCollectionTypeData.filter(element => element._id !== deletedData._id);
+        existingCollectionTypeData = existingCollectionTypeData.filter(
+            (element) => element._id !== deletedData._id
+        );
         // set back the data collection
         existingDataCollection[type] = existingCollectionTypeData;
         // re-write the data
         saveData(storagePath, existingDataCollection);
 
         // set the display response
-        let displayResponse = new Object;
+        let displayResponse = new Object();
         // Initialize the object of type
-        displayResponse[type] = new Object;
+        displayResponse[type] = new Object();
         // update with modified existing data
         displayResponse[type][id] = deletedData;
         // send the response to the client
         res.send(displayResponse);
-    })
-})
+    });
+});
 
 module.exports = storageRoutes;
